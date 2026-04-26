9b5fb6b15c11476edd5598c859b26fcde23bf56a
Francesca Vera <sally.acorn16@hotmail.com>
2026-04-26 14:41:13 -0500

    clase player base, interfas de usable y base de entity, tambien usada para hacer el jugador

---
+++ examen/examen/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace examen
+{
+    public class Player : IEntity
+    {
+        public string Name { get; set; }
+
+        public int Health { get; set; }
+
+        public int Damage { get; set; }
+
+
+        public Player(string name)
+        {
+            Name = name;
+            Health = 30;
+            Damage = 5;
+        }
+
+
+        public void Attack(IEntity target)
+        {
+            target.Health -= Damage;
+        }
+
+
+        public bool IsAlive()
+        {
+            return Health > 0;
+        }
+    }
+}
